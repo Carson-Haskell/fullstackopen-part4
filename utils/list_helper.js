8168b9777215b80513b4fc7d7856670190398f28
Carson Haskell <carson.haskell@gmail.com>
2023-06-27 15:44:01 -0700

    complete exercise 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,8 +1,23 @@
-const dummy = (array) => 1;
+const dummy = () => 1;
 
 const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum + blog.likes, 0);
+
+const favoriteBlog = (blogs) => {
+  if (!blogs.length > 0) return 'no blogs';
+
+  const favorite = blogs.reduce((mostLiked, blog) =>
+    blog.likes > mostLiked.likes ? blog : mostLiked,
+  );
+
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes,
+  };
+};
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
